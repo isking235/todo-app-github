9849a9803a03c2b8028ebe04ae861e1a7aeff3ab
isking235 <isking235@hanmail.net>
2021-04-27 22:57:51 +0900

    일부러 틀림

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, {useState,useRef, useCallback} from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
-  const [todos, setTodos] = useState([
+  const [todos, setTodos] = us_eState([
     {
-      id:1,
-      text:'리액트의 기초 알아보기',
-      checked:true,
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
     },
     {
-      id:2,
-      text:'컴포넌트 스타일링해 보기',
-      checked:true,
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
     },
     {
-      id:3,
-      text:'일정 관리 앱 만들어 보기',
-      checked : false,
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
     },
-
   ]);
 
   //고윳값으로 사용될 id
@@ -28,42 +27,42 @@ const App = () => {
   const nextId = useRef(4);
 
   const onInsert = useCallback(
-    text => {
+    (text) => {
       const todo = {
-        id : nextId.current,
+        id: nextId.current,
         text,
-        checked:false,
+        checked: false,
       };
       setTodos(todos.concat(todo));
-      nextId.current += 1;//nextId 1씩 더하기
+      nextId.current += 1; //nextId 1씩 더하기
     },
     [todos],
   );
 
   const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
     },
     [todos],
   );
 
   const onToggle = useCallback(
-    id => {
+    (id) => {
       setTodos(
-        todos.map(todo =>
-          todo.id === id? {...todo, checked: !todo.checked}:todo,
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
       );
     },
     [todos], //??
-  )
+  );
 
   return (
     <TodoTemplate>
-      <TodoInsert onInsert={onInsert}/>
-      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
-  )
+  );
 };
 
 export default App;
